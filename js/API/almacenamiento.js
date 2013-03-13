c0e6e92ee4fb10b1ab4c59557a1432e707c12b67
MiriamCuautle <cansys60@hotmail.com>
2013-03-13 14:57:53 -0600

    t

--- js/API/almacenamiento.js
+++ js/API/almacenamiento.js
@@ -76,6 +76,23 @@ function leerHistorial(){
 				);
 }
 
+
+function getId(tab){
+	var regreso=0;
+ var db = window.openDatabase("Databasem", "1.0", "HotelV2", "2000000");
+        db.transaction(function(tx) {
+					tx.executeSql('SELECT * FROM '+tab, [],	function(tx1, resultado) { //querySuccess
+							regreso=resultado.rows.length;
+						}, function(err) { //errorCB
+							//alert("Error historial1 ", "Aceptar");
+						});
+    			}, function(err) {//errorCB
+					alert("otra cosa");
+				},function(){
+				});
+				return regreso+1;
+}
+
 function leerReservas(){
  var db = window.openDatabase("Databasem", "1.0", "HotelV2", "2000000");
         db.transaction(
@@ -101,6 +118,8 @@ function leerReservas(){
 }
 
 function saveReserva(){
+	var id_reserva=getId('reserva');
+	var id_historial=getId('historial');
 	var tipoHabitacion=$('#nr1').attr('th');
 	var habit=$('#nr2 ul[data-role=listview] li:eq(1)').children('select').val();
 	var pers=$('#nr2 ul[data-role=listview] li:eq(2)').children('select').val();
@@ -109,21 +128,21 @@ function saveReserva(){
 
 	var db = window.openDatabase("Databasem", "1.0", "HotelV2", "2000000");
 	db.transaction(function (tx){
-		tx.executeSql('INSERT INTO reserva (rId , fecha, habitaciones, personas, estancia) VALUES (3, "'+fecha.getDate() + '/' + fecha.getMonth() + '/' + fecha.getFullYear()+'", "'+habit+'","'+pers+'","'+dias+'")');
+		tx.executeSql('INSERT INTO reserva (rId , fecha, habitaciones, personas, estancia) VALUES ('+id_reserva+', "'+fecha.getDate() + '/' + fecha.getMonth() + '/' + fecha.getFullYear()+'", "'+habit+'","'+pers+'","'+dias+'")');
+		tx.executeSql('INSERT INTO historial (hId , fecha, habitaciones, personas, estancia) VALUES ('+id_historial+', "'+fecha.getDate() + '/' + fecha.getMonth() + '/' + fecha.getFullYear()+'", "'+habit+'","'+pers+'","'+dias+'")');
+
 	}, function(err){
 		pgAlert('Error save', err.code);
 	},function(){
 		pgAlert('Guardado Localmente', 'Esperando por conexion al servidor');
 	});
-	
-		
-
 }
 
 
 function getData(usuario){//obtSimpleBD
 	window.localStorage.getItem(nombre);
 }
+
 
 /*
 function isLogin(){
